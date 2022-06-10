33ff6b78efaf4b47e472c94407956369c28b7bc2
Henrique Andrade <henrique.ccda@gmail.com>
2022-06-10 17:02:03 -0300

    timeline.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Cursos do site evolutio.
 
 minicurso veujs: [03/06/2022, 07/06/2022]
+djaveu: [07/06/2022, --/06/2022]
---
+++ djavue/twitter_clone/frontend/pages/perfil.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout justify-center align-center>
+    <h1> Bem vindo ao perfil </h1>
+    <!-- <v-btn color="success" :to="{name: 'todos'}">What TODO next?</v-btn> -->
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
